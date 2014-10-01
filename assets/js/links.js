9d0b5549715c8258d9b6879f73e4f35f64bd8720
Rik Schot <github@bigshotconcepts.com>
2014-10-01 14:53:23 +0200

    added all the other files
    yeahh

---
+++ assets/js/links.js
@@ -0,0 +1,64 @@
+var themes = [
+    {
+        name: 'Gridstack',
+        iframe: 'http://themewich.com/gridstack/',
+        link:'http://themeforest.net/item/gridstack-responsive-agency-wordpress-theme/5397835?ref=ResonateThemes'
+    },
+    {
+        name: 'Flatco',
+        iframe: 'http://themes.themewaves.com/flatco/',
+        link:'http://themeforest.net/item/flatco-multipurpose-responsive-wordpress-theme/5222241?ref=ResonateThemes'
+    },
+    {
+        name: 'Omega',
+        iframe: 'http://omega.oxygenna.com/',
+        link: 'http://themeforest.net/item/omega-multipurpose-responsive-bootstrap-theme/7660374?ref=ResonateThemes'
+    },
+];
+
+
+(function($) {
+
+    function selectChanged(e) {
+        var value = e.target.value;
+        var theme = themes[value];
+        $('iframe').attr('src', theme.iframe);
+        $('#btn-info').attr('href', theme.link);
+        $('#btn-buy').attr('href', theme.link);
+    }
+    //inital loading of themes
+    function init() {
+        var select = $('#theme-select');
+        select.empty();
+
+        $(themes).each(function(index,object) {
+            var selected = '';
+            if(index === 0) {
+                selected = ' selected="selected"';
+            }
+
+            var optionString = '<option value="'+index+'"'+selected+'>'+this.name+'</option>';
+            select.append($(optionString));
+        });
+
+        //change iframe and link when select changes
+        select.on('change', selectChanged);
+
+        //trigger change
+        select.change();
+
+        //update location when buy btn is clicked
+        $('#btn-buy').on('click', function(e) {
+            document.location.href = this.href;
+
+        });
+        //update location when buy btn is clicked
+        $('#btn-info').on('click', function(e) {
+            document.location.href = this.href;
+
+        });
+    }
+
+    //load
+    init();
+})(jQuery);
